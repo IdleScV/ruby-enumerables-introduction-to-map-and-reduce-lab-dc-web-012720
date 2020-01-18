d7a468354b55815ec600e84a3e6f9c672eeee91f
Wesley Chen <weschen1996@gmail.com>
2020-01-18 22:18:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,10 +20,9 @@ def reduce_to_total(src, x=0)
 end
 
 def reduce_to_all_true(src)
-  src.reduce{|x, i| i == TRUE 
-     count += 1
+  return src.reduce{|x, i| i == TRUE 
   }
-  return  count ==  src.length
+ 
 end
 
 def reduce_to_any_true(src)
