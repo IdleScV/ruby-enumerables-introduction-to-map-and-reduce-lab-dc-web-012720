a227e8fb45930445ee0293c6ec7878ef1d3a2287
Wesley Chen <weschen1996@gmail.com>
2020-01-18 22:12:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
 # My Code here....
+def map_to_negativize(src)
+  return src.map{|num| num * -1}
+end
+
+def map_to_no_change(src)
+  return src.map{|num| num}
+end
+
+def map_to_double(src)
+  return src.map{|num| num * 2}
+end
+
+def map_to_square(src)
+  return src.map{|num| num ** 2}
+end
+
+def reduce_to_total(src, x=0)
+  return src.reduce(:+) + x
+end
+
+def reduce_to_all_true(src)
+ src.reduce{|x| if x == FALSE
+  return FALSE
+ }
+end
+
+def reduce_to_any_true(src)
+ return src.reduce{ x }
+end
+
+
